4602c06ab1c5ae748b3b3e2652f5983fb33a0e8d
mohammad mazaheri <53862625+xmsa@users.noreply.github.com>
2019-11-24 21:06:20 +0330

    add code Btn frm

--- Fibonacci/FrmMain.Designer.cs
+++ Fibonacci/FrmMain.Designer.cs
@@ -35,24 +35,25 @@
             this.btnStop = new System.Windows.Forms.Button();
             this.chBoxLoop = new System.Windows.Forms.CheckBox();
             this.chBoxRecursive = new System.Windows.Forms.CheckBox();
-            this.MyAlgorithm = new System.Windows.Forms.CheckBox();
+            this.chBoxMyAlgorithm = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // txtNumber
             // 
             this.txtNumber.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNumber.Location = new System.Drawing.Point(12, 12);
+            this.txtNumber.Location = new System.Drawing.Point(32, 29);
+            this.txtNumber.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.txtNumber.Name = "txtNumber";
-            this.txtNumber.Size = new System.Drawing.Size(150, 39);
+            this.txtNumber.Size = new System.Drawing.Size(393, 86);
             this.txtNumber.TabIndex = 0;
-            this.txtNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNumber_KeyPress);
             // 
             // btnStart
             // 
             this.btnStart.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.Location = new System.Drawing.Point(12, 57);
+            this.btnStart.Location = new System.Drawing.Point(32, 136);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(102, 38);
+            this.btnStart.Size = new System.Drawing.Size(272, 91);
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -61,9 +62,10 @@
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(190, 56);
+            this.btnExit.Location = new System.Drawing.Point(507, 134);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(64, 38);
+            this.btnExit.Size = new System.Drawing.Size(171, 91);
             this.btnExit.TabIndex = 1;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -73,29 +75,32 @@
             // 
             this.lblNumber.AutoSize = true;
             this.lblNumber.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNumber.Location = new System.Drawing.Point(168, 15);
+            this.lblNumber.Location = new System.Drawing.Point(448, 36);
+            this.lblNumber.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblNumber.Name = "lblNumber";
-            this.lblNumber.Size = new System.Drawing.Size(86, 38);
+            this.lblNumber.Size = new System.Drawing.Size(213, 95);
             this.lblNumber.TabIndex = 2;
             this.lblNumber.Text = "Number";
             // 
             // btnStop
             // 
             this.btnStop.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStop.Location = new System.Drawing.Point(120, 56);
+            this.btnStop.Location = new System.Drawing.Point(320, 134);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(64, 38);
+            this.btnStop.Size = new System.Drawing.Size(171, 91);
             this.btnStop.TabIndex = 1;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
-            this.btnStop.Click += new System.EventHandler(this.BtnExit_Click);
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
             // 
             // chBoxLoop
             // 
             this.chBoxLoop.AutoSize = true;
-            this.chBoxLoop.Location = new System.Drawing.Point(261, 19);
+            this.chBoxLoop.Location = new System.Drawing.Point(696, 45);
+            this.chBoxLoop.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.chBoxLoop.Name = "chBoxLoop";
-            this.chBoxLoop.Size = new System.Drawing.Size(50, 17);
+            this.chBoxLoop.Size = new System.Drawing.Size(117, 36);
             this.chBoxLoop.TabIndex = 3;
             this.chBoxLoop.Text = "Loop";
             this.chBoxLoop.UseVisualStyleBackColor = true;
@@ -103,29 +108,31 @@
             // chBoxRecursive
             // 
             this.chBoxRecursive.AutoSize = true;
-            this.chBoxRecursive.Location = new System.Drawing.Point(261, 42);
+            this.chBoxRecursive.Location = new System.Drawing.Point(696, 100);
+            this.chBoxRecursive.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.chBoxRecursive.Name = "chBoxRecursive";
-            this.chBoxRecursive.Size = new System.Drawing.Size(74, 17);
+            this.chBoxRecursive.Size = new System.Drawing.Size(179, 36);
             this.chBoxRecursive.TabIndex = 4;
             this.chBoxRecursive.Text = "Recursive";
             this.chBoxRecursive.UseVisualStyleBackColor = true;
             // 
-            // MyAlgorithm
+            // chBoxMyAlgorithm
             // 
-            this.MyAlgorithm.AutoSize = true;
-            this.MyAlgorithm.Location = new System.Drawing.Point(261, 63);
-            this.MyAlgorithm.Name = "MyAlgorithm";
-            this.MyAlgorithm.Size = new System.Drawing.Size(83, 17);
-            this.MyAlgorithm.TabIndex = 5;
-            this.MyAlgorithm.Text = "MyAlgorithm";
-            this.MyAlgorithm.UseVisualStyleBackColor = true;
+            this.chBoxMyAlgorithm.AutoSize = true;
+            this.chBoxMyAlgorithm.Location = new System.Drawing.Point(696, 150);
+            this.chBoxMyAlgorithm.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.chBoxMyAlgorithm.Name = "chBoxMyAlgorithm";
+            this.chBoxMyAlgorithm.Size = new System.Drawing.Size(211, 36);
+            this.chBoxMyAlgorithm.TabIndex = 5;
+            this.chBoxMyAlgorithm.Text = "MyAlgorithm";
+            this.chBoxMyAlgorithm.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 100);
-            this.Controls.Add(this.MyAlgorithm);
+            this.ClientSize = new System.Drawing.Size(971, 238);
+            this.Controls.Add(this.chBoxMyAlgorithm);
             this.Controls.Add(this.chBoxRecursive);
             this.Controls.Add(this.chBoxLoop);
             this.Controls.Add(this.lblNumber);
@@ -134,6 +141,7 @@
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.txtNumber);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMain";
@@ -152,7 +160,7 @@
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.CheckBox chBoxLoop;
         private System.Windows.Forms.CheckBox chBoxRecursive;
-        private System.Windows.Forms.CheckBox MyAlgorithm;
+        private System.Windows.Forms.CheckBox chBoxMyAlgorithm;
     }
 }
 
--- Fibonacci/FrmMain.cs
+++ Fibonacci/FrmMain.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Fibonacci
@@ -15,21 +16,181 @@ namespace Fibonacci
         public frmMain()
         {
             InitializeComponent();
+            btnStop.Enabled = false;
+            fibonacci = new CFibonacci(0);
+            tLoop = new Thread(fibonacci.Loop);
+            tRecursive = new Thread(fibonacci.Recursive);
+            tMyAlgorithm = new Thread(fibonacci.MyAlgorithm);
         }
+
+        CFibonacci fibonacci;
+
+        Thread tLoop ;
+        Thread tRecursive ;
+        Thread tMyAlgorithm ;
 
         private void BtnStart_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (txtNumber.TextLength<1)
+                {
+                    throw new Exception();
+                }
+                int number = Convert.ToInt32(txtNumber.Text);
+                Start(number);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("only number");
+                txtNumber.Text = string.Empty;
+                txtNumber.Focus();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Enter a Number");
+                txtNumber.Focus();
+            }
         }
 
         private void BtnExit_Click(object sender, EventArgs e)
         {
+            Application.Exit();
+        }
+        enum State
+        {
+            Loop,
+            Recursive,
+            MyAlgorithm,
+            Loop_Recursive,
+            Loop_MyAlgorithm,
+            Recursive_MyAlgorithm,
+            All,
+            None
+        }
+
+        void Start(int number)
+        {
+            fibonacci.number = number;
+
+            btnExit.Enabled = false;
+            btnStop.Enabled = true;
+            btnStart.Enabled = false;
+            chBoxLoop.Enabled = false;
+            chBoxMyAlgorithm.Enabled = false;
+            chBoxRecursive.Enabled = false;
+
+
+            State s = Switch();
+            switch (s)
+            {
+                case State.Loop:
+                    tLoop.Start();
+
+                    tLoop.Join();
+                    break;
+                case State.Recursive:
+                    tRecursive.Start();
+
+                    tRecursive.Join();
+                    break;
+                case State.MyAlgorithm:
+                    tMyAlgorithm.Start();
+
+                    tMyAlgorithm.Join();
+                    break;
+                case State.Loop_Recursive:
+                    tLoop.Start();
+                    tRecursive.Start();
+
+                    tRecursive.Join();
+                    tMyAlgorithm.Join();
+                    break;
+                case State.Loop_MyAlgorithm:
+                    tLoop.Start();
+                    tMyAlgorithm.Start();
+
+                    tMyAlgorithm.Join();
+                    tLoop.Join();
+                    break;
+                case State.Recursive_MyAlgorithm:
+                    tRecursive.Start();
+                    tMyAlgorithm.Start();
+
+                    tRecursive.Join();
+                    tMyAlgorithm.Join();
+                    break;
+                case State.All:
+                    tLoop.Start();
+                    tRecursive.Start();
+                    tMyAlgorithm.Start();
+
+                    tLoop.Join();
+                    tMyAlgorithm.Join();
+                    tLoop.Join();
+                    break;
+                case State.None:
+                    MessageBox.Show("Plase Enter one item");
+                    break;
+            }
+            btnExit.Enabled = true;
+            btnStart.Enabled = true;
+            chBoxLoop.Enabled = true;
+            chBoxMyAlgorithm.Enabled = true;
+            chBoxRecursive.Enabled = true;
+            btnStop.Enabled = false;
 
         }
 
-        private void TxtNumber_KeyPress(object sender, KeyPressEventArgs e)
+        private State Switch()
         {
+            if (chBoxLoop.Checked && chBoxRecursive.Checked && chBoxMyAlgorithm.Checked) 
+            {
+                return State.All;
+            }
+            else if (chBoxLoop.Checked && !chBoxRecursive.Checked && !chBoxMyAlgorithm.Checked) 
+            {
+                return State.Loop;
+            }
+            else if (!chBoxLoop.Checked && chBoxRecursive.Checked && !chBoxMyAlgorithm.Checked) 
+            {
+                return State.Recursive;
+            }
+            else if (chBoxLoop.Checked && chBoxRecursive.Checked && !chBoxMyAlgorithm.Checked) 
+            {
+                return State.Loop_Recursive;
+            }
+            else if (!chBoxLoop.Checked && !chBoxRecursive.Checked && chBoxMyAlgorithm.Checked) 
+            {
+                return State.MyAlgorithm;
+            }
+            else if (chBoxLoop.Checked && !chBoxRecursive.Checked && chBoxMyAlgorithm.Checked) 
+            {
+                return State.Loop_MyAlgorithm;
+            }
+            else if (!chBoxLoop.Checked && chBoxRecursive.Checked && chBoxMyAlgorithm.Checked) 
+            {
+                return State.Recursive_MyAlgorithm;
+            }
+            else
+            {
+                return State.None;
+            }
+            
+        }
 
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            tLoop.Abort();
+            tMyAlgorithm.Abort();
+            tLoop.Abort();
+
+            btnExit.Enabled = true;
+            btnStart.Enabled = true;
+            chBoxLoop.Enabled = true;
+            chBoxMyAlgorithm.Enabled = true;
+            chBoxRecursive.Enabled = true;
+            btnStop.Enabled = false;
         }
     }
 }
